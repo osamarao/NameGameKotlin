7e7934a94d9c4472ce889ccfa8aee191f5440fe2
osamarao <osama.rao@willowtreeapps.com>
2018-01-02 14:09:08 -0500

    UDFing....

--- app/src/main/java/osama/me/namegamekotlin/MainActivity.kt
+++ app/src/main/java/osama/me/namegamekotlin/MainActivity.kt
@@ -6,14 +6,15 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import tw.geothings.rekotlin.Store
+import tw.geothings.rekotlin.StoreSubscriber
 
-
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), StoreSubscriber<AppState> {
 
     private val container by lazyFindView<ViewGroup>(R.id.face_container)
     private val xtext by lazyFindView<TextView>(R.id.xtext)
 
-    private var pairs: ArrayList<Pair<PersonViewModel, ImageView>> = arrayListOf()
+    //        private var pairs_: ArrayList<Pair<PersonViewModel, ImageView>> = arrayListOf()
     private var targetPair: Pair<PersonViewModel, ImageView>? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,34 +28,44 @@ class MainActivity : AppCompatActivity() {
             it.setOnClickListener {
                 if ((it as ImageView).pair == targetPair) {
                     it.visibility = View.GONE
-                    pairs.remove(targetPair!!)
-                    setTarget()
+                    mainStore.dispatch(RemovePairAction(it.pair))
                 } else {
                     it.shake()
                 }
 
             }
         }
-
-        ProfileRepository.ProfileRepositoryProvider.provideProfileRepository().profiles().exec({ people ->
-            people.let { pickRandomN(it, 5) }.map { it.viewmodel }.zip(faces).onEach { pairs.add(it) }.forEach {
-                it.second.apply { setSize(dpToPixel(100f), dpToPixel(100f)) }.loadUrl(it.first.headshot.trueUrl)
-                it.second.pair = it // ???
-            }
-            setTarget()
-        }, { error ->
-            logDebug { error.message }
-        })
+        mainStore.subscribe(this)
+        profilePairsActionCreator(faces, this)
     }
 
-
-    private fun setTarget() {
-        if (!pairs.isEmpty()) {
-            targetPair = pickRandomN(pairs, 1)[0]
+    private fun setTarget(pairs: List<Pair<PersonViewModel, ImageView>>) {
+        targetPair = if (!pairs.isEmpty()) {
+            pickRandomN(pairs, 1)[0]
         } else {
-            targetPair = null
+            null
         }
         xtext.text = targetPair?.first?.name ?: "No Target"
     }
 
+
+    override fun newState(state: AppState) {
+        state.pairs.forEach {
+            logDebug { it.first.name }
+            it.second.apply { setSize(dpToPixel(100f), dpToPixel(100f)) }.loadUrl(it.first.headshot.trueUrl)
+            it.second.pair = it // add to tag of imageview
+        }
+        setTarget(state.pairs)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mainStore.unsubscribe(this)
+    }
+
 }
+
+val mainStore = Store(
+        reducer = ::pairReducer,
+        state = null
+)
---
+++ app/src/main/java/osama/me/namegamekotlin/Redux.kt
@@ -0,0 +1,34 @@
+@file:Suppress("UNCHECKED_CAST")
+
+package osama.me.namegamekotlin
+
+import android.widget.ImageView
+import tw.geothings.rekotlin.Action
+import tw.geothings.rekotlin.StateType
+
+data class AppState(val pairs: ArrayList<Pair<PersonViewModel, ImageView>> = arrayListOf()) : StateType
+
+
+data class ReplacePairsAction(val pairs: List<Pair<PersonViewModel, ImageView>>) : Action
+data class RemovePairAction(val pair: Pair<PersonViewModel, ImageView>) : Action
+
+fun pairReducer(action: Action, state: AppState?): AppState {
+
+    var stateInternal = state ?: AppState()
+
+    when (action) {
+        is ReplacePairsAction -> {
+            if (state != null) {
+                stateInternal = state.copy(pairs = action.pairs as ArrayList<Pair<PersonViewModel, ImageView>>)
+            }
+        }
+        is RemovePairAction -> {
+            if (state != null) {
+                val clonePairs = state.pairs.clone() as ArrayList<Pair<PersonViewModel, ImageView>>
+                clonePairs.remove(action.pair)
+                stateInternal = state.copy(pairs = clonePairs)
+            }
+        }
+    }
+    return stateInternal
+}
